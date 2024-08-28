0aa9eb47a5bd9a51ba8e97786aaa40a112fcf4b6
MauryIV <mauryhughesiv@gmail.com>
2024-08-27 19:11:33 -0600

    Preperation for adding controllers in routes

---
+++ server/routes/api/adminRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {
+// include controller functions here
+} = require('../../controllers/adminController.js');
+
+router.route('/')
+// include controllers here
+  .put();
+
+module.exports = router;
